defcfc700755554b92025ec90f650700943e435c
anamulislamshamim <anamulislamshamim117@gmail.com>
2024-06-29 16:59:45 +0600

    configure .env file

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool)
+DEBUG = config("DJANGO_DEBUG", cast=bool)
 
 ALLOWED_HOSTS = [
     # https://saas.prod.railway.app
